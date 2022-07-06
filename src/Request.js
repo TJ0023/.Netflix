774d9867ab00b20a2065a662085c8393df5ac867
TJ0023 <GimenezTJV@gmail.com>
2022-07-06 13:29:12 +0800

    Let's just say that some results in the action query was questionnable. I've replaced it with Now Playing instead

--- src/Request.js
+++ src/Request.js
@@ -4,7 +4,7 @@ const requests = {
     requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestAction:`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=action&page=1&include_adult=false`,
+    requestNowPlaying:`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=3`,
     requestUpcoming:`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
 };
 
